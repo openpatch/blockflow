27cfe08f74d40e5f44131b7a070bebec69bf04e8
Mike Barkmin <mike@barkmin.eu>
2026-03-01 20:53:04 +0100

    fix: update logo url

--- packages/scratch-gui/src/playground/render-gui.jsx
+++ packages/scratch-gui/src/playground/render-gui.jsx
@@ -9,7 +9,7 @@ import log from '../lib/log.js';
 import {PLATFORM} from '../lib/platform.js';
 
 const onClickLogo = () => {
-    window.location = 'https://scratch.mit.edu';
+    window.location = 'https://blockflow.openpatch.org';
 };
 
 const handleTelemetryModalCancel = () => {
